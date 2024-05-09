7def94341919b5e3347918a71ce7c19de6b27216
Md. Mamun Or Rashid <mamun.rashid@infolytx.com>
2024-05-09 11:44:01 +0600

    update all functions unit test

--- index.test.js
+++ index.test.js
@@ -1,34 +1,58 @@
 const { reverse, removeDuplicates, flatten } = require('./index');
 
 describe('test all the functions', () => {
-    describe("reverse", () => {
-        test('hello equal to olleh', () => {
+    describe('reverse function', () => {
+        test('should reverse the string', () => {
             expect(reverse("hello")).toBe("olleh");
         });
 
-        test("should not return the non reverse string", () => {
-            expect(reverse("hello")).not.toBe("hello");
-        })
-    })
-    describe("removeDuplicates", () => {
-        test('This is is a test test  string equal to This is a test  string', () => {
-            expect(removeDuplicates("This is is a test test  string")).toBe("This is a test  string");
+        test('should handle empty string', () => {
+            expect(reverse("")).toBe("");
         });
 
-        test("should not return duplicates string", () => {
-            expect(removeDuplicates("hello hello")).not.toBe("hello hello");
-        })
-    })
+        test('should handle string with single character', () => {
+            expect(reverse("a")).toBe("a");
+        });
+
+        test('should handle string with numbers and special characters', () => {
+            expect(reverse("123!@#")).toBe("#@!321");
+        });
+
+        test('should not return the original string', () => {
+            expect(reverse("hello")).not.toBe("hello");
+        });
+    });
+    describe('removeDuplicates function', () => {
+        test('should remove duplicate words from a string', () => {
+            const inputString = "hello world world hello";
+            const expectedOutput = "hello world";
+            expect(removeDuplicates(inputString)).toEqual(expectedOutput);
+        });
+
+        test('should handle empty string', () => {
+            const inputString = "";
+            const expectedOutput = "";
+            expect(removeDuplicates(inputString)).toEqual(expectedOutput);
+        });
+
+        test('should handle string with no duplicates', () => {
+            const inputString = "apple banana orange";
+            const expectedOutput = "apple banana orange";
+            expect(removeDuplicates(inputString)).toEqual(expectedOutput);
+        });
+
+        test('should handle string with all duplicates', () => {
+            const inputString = "hello hello hello";
+            const expectedOutput = "hello";
+            expect(removeDuplicates(inputString)).toEqual(expectedOutput);
+        });
+    });
     describe("flatten", () => {
         test('[1, [2, [3, 4], 5], 6] equal [1, 2, 3, 4, 5, 6] ', () => {
             const nestedArray = [1, [2, [3, 4], 5], 6];
             const expected = [1, 2, 3, 4, 5, 6];
             expect(flatten(nestedArray)).toEqual(expected);
         });
-
-        // test('[1, 2, [4, 5]] equal [1, 2, 4, 5] ', () => {
-        //     expect(flatten([1, 2, [4, 5]])).not.toBe([1, 2, [4, 5]]);
-        // });
     })
 
     test('should handle an empty array', () => {
